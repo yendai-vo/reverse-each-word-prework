1cd746cd417169fcb6b7c9f69d219fa57e0e5fd0
Cindy Vo <yendai.vo@hotmail.com>
2018-07-12 19:43:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   string.reverse.split.reverse.join(" ")
 end
 
-def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+def reverse_each_word(string)
+  string.split.collect {|word| word.reverse}.join(" ")
 end
